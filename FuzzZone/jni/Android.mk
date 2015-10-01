9d16dbef262042f955554f07ef4a6770a1f37003
Ryan Welton <ryan.g.welton@gmail.com>
2015-10-01 17:16:04 -0400

    Add runner script + make PIE

--- FuzzZone/jni/Android.mk
+++ FuzzZone/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := fuzz_zone
-LOCAL_CFLAGS += -std=c99
-LOCAL_LDLIBS := -llog
+LOCAL_CFLAGS += -std=c99 -fpie -pie
+LOCAL_LDLIBS := -llog -pie
 LOCAL_SRC_FILES := main.c
 include $(BUILD_EXECUTABLE)
